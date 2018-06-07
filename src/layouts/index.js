9a909a365381907a25d3a464801006393fb97a8a
Kris Ciccarello <kmctown@users.noreply.github.com>
2018-06-07 18:50:31 -0400

    Reduce home header font from h3 to h2

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -42,7 +42,7 @@ class Template extends React.Component {
 
     if (location.pathname === rootPath) {
       header = (
-        <h1
+        <h2
           style={{
             ...scale(1.5),
             marginBottom: rhythm(1.5),
@@ -59,7 +59,7 @@ class Template extends React.Component {
           >
             Building Wisr
           </Link>
-        </h1>
+        </h2>
       )
     } else {
       header = (
